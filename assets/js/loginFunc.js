41d1ae8996d7003a7b4ccfcafb1db10988b2f0c3
dominiclake <dominiclake@oakland.edu>
2025-03-18 16:37:15 -0400

    added state management for homepage (index.html)

--- assets/js/loginFunc.js
+++ assets/js/loginFunc.js
@@ -4,21 +4,28 @@ $(document).ready(function () {
     const logoutBtn = $('#logout-btn');
     const loginLink = $('.login-link');
     const registerLink = $('.register-link');
+    const guestUser = $('#guest_user');
+    const verifiedUser = $('#verified_user');
+
 
     function updateAuthUI() {
         const user = JSON.parse(localStorage.getItem('user'));
 
         if (user && user.name) {
-            userNameDisplay.text(`Welcome, ${user.name}`);
-            userNameDisplay.show();
-            logoutBtn.show();
-            loginLink.hide();
-            registerLink.hide();
+          userNameDisplay.text(`Welcome, ${user.name}`);
+          //  userNameDisplay.show();
+          //  logoutBtn.show();
+          //  loginLink.hide();
+          //  registerLink.hide();
+          guestUser.hide();
+          verifiedUser.show();
         } else {
-            userNameDisplay.hide();
-            logoutBtn.hide();
-            loginLink.show();
-            registerLink.show();
+          //  userNameDisplay.hide();
+          //  logoutBtn.hide();
+          //  loginLink.show();
+          //  registerLink.show();
+          guestUser.show();
+          verifiedUser.hide();
         }
     }
 
